1324b2111eeb9d7bb84ca6a997fcea3ea7079625
Tyler Lindow <tyler.lindow@gmail.com>
2019-03-17 22:54:29 -0700

    Make app a react component
    Add searchMovieList function and Search bar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,40 @@
+import movies from "../data/exampleMovieData.js";
 import NavBar from "./NavBar.js"
+import MovieList from "./MovieList.js";
+import Search from "./Search.js";
 
-var App = () => (
-  <div>
-    < NavBar/>
-  </div>
-)
+class App extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      movies: movies
+    }
+  }
+
+  searchMovieList (query) {
+    var movieTitles = movies.map((movie) => {
+      return movie.title;
+    });
+    if (movieTitles.includes(query)) {
+      this.setState({
+        movies: [movies[0]]
+      })
+    } else {
+      this.setState({
+        movies: movies
+      })
+    }
+  }
+
+  render() {
+    return <div>
+      <NavBar />
+      <Search searchMovieList={this.searchMovieList.bind(this)}/>
+      <MovieList movies={this.state.movies} />
+    </div>
+  }
+
+}
 
 export default App;
