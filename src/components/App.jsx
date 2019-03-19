20cd53d452b35fc100336a98761e522fff7697f3
Hack Reactor Student <student@hackreactor.com>
2019-03-18 20:52:39 -0700

    Add searchMovieList functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import movies from "../data/exampleMovieData.js";
-import NavBar from "./NavBar.js"
+import NavBar from "./NavBar.js";
+import AddMovies from "./AddMovies.js";
 import MovieList from "./MovieList.js";
 import Search from "./Search.js";
 
@@ -16,20 +17,41 @@ class App extends React.Component {
     var movieTitles = movies.map((movie) => {
       return movie.title;
     });
-    if (movieTitles.includes(query)) {
-      this.setState({
-        movies: [movies[0]]
-      })
-    } else {
-      this.setState({
-        movies: movies
-      })
+
+    var filteredTitles = movieTitles.filter((title) => {
+      return title.includes(query);
+    });
+
+    var filteredMovies = [];
+    for (var i = 0; i < movies.length; i++) {
+      for (var j = 0; j < filteredTitles.length; j++) {
+        if (movies[i].title === filteredTitles[j]) {
+          filteredMovies.push(movies[i]);
+        }
+      }
     }
+ 
+    this.setState({
+      movies: filteredMovies
+    });
+  }
+
+  addMovie (entry) {
+    var newMovie = {
+      title: entry
+    };
+    var moviesCopy = movies.slice();
+    moviesCopy.push(newMovie);
+    console.log(movies);
+    this.setState({
+      movies: moviesCopy
+    });
   }
 
   render() {
     return <div>
       <NavBar />
+      <AddMovies addMovie={this.addMovie.bind(this)}/>
       <Search searchMovieList={this.searchMovieList.bind(this)}/>
       <MovieList movies={this.state.movies} />
     </div>
